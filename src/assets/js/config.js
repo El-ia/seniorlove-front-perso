25f44127c1fff0b6728725879954a952aadc9a11
Elia Berthier <eliaberthier@MacBook-Air-de-Elia.local>
2025-04-07 18:00:04 +0200

    change apiUrl

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,1 +1,2 @@
-export const apiUrl = "https://senior-love-back-8.onrender.com/api";
+// export const apiUrl = "https://senior-love-back-8.onrender.com/api";
+export const apiUrl = "http://localhost:3000/api";
